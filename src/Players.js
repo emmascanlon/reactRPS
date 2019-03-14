8eec81fdc0ed8d56b0ef151886803921ab671762
Emma Scanlon <emmascanlon4@gmail.com>
2019-03-14 16:30:52 -0600

    added some more buttons but now it's not recognizing ties

--- src/Players.js
+++ src/Players.js
@@ -1,64 +1,106 @@
 import React, { Component } from 'react';
 import Result from './Result'
-import { Button, Container, Segment, Header} from "semantic-ui-react";
+import './App.css';
+import { Button, Container, Segment, Header, Icon} from "semantic-ui-react";
 import { blue } from 'ansi-colors';
 
 
 class Players extends Component {
-  state = { player1: undefined, player2: undefined }
+  state = { player1: undefined, player2: undefined, choiceMade1: false, choiceMade2: false, }
 
   Rock1 = () => {
     this.setState( state => {
-      return {player1: "rock"}
+      return {player1: "rock"}});
+      this.setState( state => {
+        return {choiceMade1: true}})
     }
-    )}
+    
 
   Paper1 = () => {
     this.setState( state => {
-      return {player1: "paper"}
+      return {player1: "paper"}})
+      this.setState( state => {
+        return {choiceMade1: true}})
     }
-    )}
+  
 
   Scissors1 = () => {
     this.setState( state => {
-      return {player1: "scissors"}
+      return {player1: "scissors"}})
+      this.setState( state => {
+        return {choiceMade1: true}})
     }
-    )}
+  
 
   Rock2 = () => {
     this.setState( state => {
-      return {player2: "rock"}
+      return {player2: "rock"}})
+      this.setState( state => {
+        return {choiceMade2: true}})
     }
-    )}
+    
   
   Paper2 = () => {
     this.setState( state => {
-      return {player2: "paper"}
+      return {player2: "paper"}})
+      this.setState( state => {
+        return {choiceMade2: true}})
     }
-    )}
+  
 
   Scissors2 = () => {
     this.setState( state => {
-      return {player2: "scissors"}
+      return {player2: "scissors"}})
+      this.setState( state => {
+        return {choiceMade2: true}})
     }
-    )}
+  
+    resetChoice1 = () => {
+      this.setState( state => {
+        return {choiceMade1: false}})
+      }
+
+    resetChoice2 = () => {
+      this.setState( state => {
+        return {choiceMade2: false}})
+      }
 
   render() {
     return (
-      <Container style={styles.container}>
-        <Segment style = {styles.segement}> 
+      <Container id="container" style={styles.container}>
+        <div id="players">
+        <div>
+        <Segment id = "player1" style = {styles.segement}> 
           <Header style={styles.header}>Player 1</Header>
-          <Button onClick={this.Rock1}>Rock</Button>
-          <Button onClick={this.Paper1}>Paper</Button>
-          <Button onClick={this.Scissors1}>Scissors</Button>
-          </Segment>
-    <Result Player1={this.state.player1} Player2={this.state.player2}/>
-       <Segment>
-         <Header> Player 2</Header>
-       <Button onClick={this.Rock2}>Rock</Button>
-        <Button onClick={this.Paper2}>Paper</Button>
-        <Button onClick={this.Scissors2}>Scissors</Button>
+    <div> {this.state.choiceMade1 ? <Button onClick={this.resetChoice1}>Change your Choice</Button> :
+          <div><Button color="red" onClick={this.Rock1}><a><Icon color="white" size="huge" name="hand rock outline inverted"/></a></Button>
+          <Button color="red" onClick={this.Paper1}><a><Icon color="white"size="huge" name="hand paper outline inverted"/></a></Button>
+          <Button color="red" onClick={this.Scissors1}><a><Icon color="white" size="huge" name="hand scissors outline inverted"/></a></Button></div> }
+        </div>
         </Segment>
+        </div>
+        <div>
+        <Segment id= "player2">
+          <Header> Player 2</Header>
+          <div> {this.state.choiceMade2 ? <Button onClick={this.resetChoice2}>Change your Choice</Button> :
+          <div>
+          <Button color="red" onClick={this.Rock2}><a><Icon color="white"size="huge" name="hand rock outline inverted"/></a></Button>
+          <Button color="red" onClick={this.Paper2}><a><Icon color="white" size="huge" name="hand paper outline inverted"/></a></Button>
+          <Button color="red" onClick={this.Scissors2}><a><Icon color="white" size="huge" name="hand scissors outline inverted"/></a></Button>
+           </div>}
+          </div>
+        </Segment>
+        </div>
+
+        </div>
+
+        <div>
+        <Segment id = "result">
+          <Result  Player1={this.state.player1} Player2={this.state.player2} resetChoice1={this.resetChoice1} resetChoice2={this.resetChoice2}/>
+        </Segment>
+        </div>
+        
+       
 
       </Container>
     );
--- src/Result.js
+++ src/Result.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Button, Container, Segment, Header, Table} from "semantic-ui-react";
+import { Button, Container, Segment, Table} from "semantic-ui-react";
+import './App.css';
 
 class Result extends Component {
   state = {
@@ -7,99 +8,123 @@ class Result extends Component {
     Losses: 0, 
     Ties: 0,
     winner: undefined,
+    winnerDeclared: false
    }
   
 
   setWinner = () => {
-    const {Player1, Player2} = this.props
-    const {Wins, Losses, Ties, Winner } = this.state
+    const {Player1, Player2,} = this.props;
+    const {Wins, Losses, Ties,} = this.state;
+    this.setState({winnerDeclared: true})
+   
   if (Player1 === Player2) {
     this.setState({Winner: "It's a Tie!"}) 
     this.setState({Ties: Ties +1})
-  
+
   }
 
     else if (Player1 === 'rock' && Player2 === 'scissors')
     {this.setState({winner: 'Player1 Wins!'})
       this.setState({Wins: Wins+1 }) 
-      
     }
 
     else if (Player1 === 'rock' && Player2 === 'paper')
     {this.setState({winner: 'Player2 Wins!' }) 
     this.setState({Losses: Losses+1 }) 
-   
   }
 
     else if (Player1 === "paper" && Player2 === "scissors")
     {this.setState({winner: 'Player2 Wins!'}) 
     this.setState({Losses: Losses+1 }) 
-    
   }
 
     else if (Player1 === "paper" && Player2 === "rock")
     {this.setState({winner: 'Player1 Wins!'}) 
     this.setState({Wins: Wins+1 }) 
-   
   }
 
     else if (Player1 === "scissors" && Player2 === "rock")
     {this.setState({winner: 'Player2 Wins!'}) 
     this.setState({Losses: Losses+1})
- 
   }
 
     else if (Player1 === "scissors" && Player2 === "paper")
     {this.setState({winner: 'Player1 Wins!'}) 
     this.setState({Wins: Wins+1 }) 
-   
   }
-    
+}
+
+  resetScoreBoard = () => {
+    this.setState({Wins: 0, Ties: 0, Losses: 0})
   }
+
+  newRound = () => {
+    const {resetChoice1, resetChoice2} =this.props
+    this.setState({winnerDeclared: false})
+    resetChoice1()
+    resetChoice2()
+  }
+
+
+  
 
   render() {
-    const {Wins, Losses, Ties, winner } = this.state
+    const {Wins, Losses, Ties, winner, winnerDeclared } = this.state
+    const {Player1, Player2,} = this.props
     return (
       <Container>
-        <Segment>
-        <h4>Player 1</h4>
-        <Table>
-          <tr>
-            <td>Wins</td>
-            <td>Losses</td>
-            <td>Ties</td>
-          </tr>
-          <tr>
-            <td>{Wins}</td>
-            <td>{Losses}</td>
-            <td>{Ties}</td>
-          </tr>
-        </Table>
-      </Segment>
-
-     <Segment>
-      <h4>Player 2</h4>
-        <Table>
-          <tr>
-            <td>Wins</td>
-            <td>Losses</td>
-            <td>Ties</td>
-          </tr>
-          <tr>
-            <td>{Losses}</td>
-            <td>{Wins}</td>
-            <td>{Ties}</td>
-          </tr>
-        </Table>
-      </Segment>
+<div> 
+{ winnerDeclared ?  <div>
+        <h5> Player1 chose {Player1} </h5>
+        <h5> Player2 chose {Player2} </h5>
   
        <h1>
          {winner}
        </h1>
+       </div> : null }
+
+       </div>
      
      <Button onClick={this.setWinner} color="violet">Shoot!</Button>
-     </Container>
+     <Button onClick={this.newRound} color="olive">New Round</Button>
+     <Button onClick={this.resetScoreBoard} color="yellow">Reset Scores</Button>
+    
+     
+     
 
+<Segment>
+<h4>Player 1</h4>
+<Table>
+  <tr>
+    <td>Wins</td>
+    <td>Losses</td>
+    <td>Ties</td>
+  </tr>
+  <tr>
+    <td>{Wins}</td>
+    <td>{Losses}</td>
+    <td>{Ties}</td>
+  </tr>
+</Table>
+</Segment>
+
+<Segment>
+<h4>Player 2</h4>
+<Table>
+  <tr>
+    <td>Wins</td>
+    <td>Losses</td>
+    <td>Ties</td>
+  </tr>
+  <tr>
+    <td>{Losses}</td>
+    <td>{Wins}</td>
+    <td>{Ties}</td>
+  </tr>
+</Table>
+</Segment>
+     
+</Container>
     );
   }
 }
